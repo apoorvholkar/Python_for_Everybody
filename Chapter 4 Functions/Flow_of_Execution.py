b6779f69b4f7cfa6a2072baf48b281df8831ee56
apoorv <apoorvholkar007@gmail.com>
2018-03-11 11:31:11 +0800

    Flow of execution

---
+++ Chapter 4 Functions/Flow_of_Execution.py
@@ -0,0 +1,7 @@
+"""
+Execution begins from the first statement of the program.
+Statements are executed one at a time from top to bottom.
+
+Statements inside the function are not executed until the function is called.
+
+"""
