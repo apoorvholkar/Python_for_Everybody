11c65306aa4a9ba31546a27ae71e1138e980bf83
apoorv <apoorvholkar007@gmail.com>
2018-03-16 04:26:33 +0800

    Flow of execution

---
+++ Chapter 4 Functions/Parameters_Arguments.py
@@ -0,0 +1,9 @@
+"""
+argument: the value passed to a function, at the time of function call
+parameters: inside the function the arguments are assigned to variables called parameters
+
+Fruitful functions: functions that return a value, eg. math functions
+Void functions: functions that perform a action, eg. function to print a argument twice.
+
+
+"""
